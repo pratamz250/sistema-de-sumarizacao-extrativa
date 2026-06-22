078942464bc3c770ddf90f82a644d474649593d6
Dara Maria <67131700+daramariabs@users.noreply.github.com>
2026-06-22 12:16:38 -0300

    fix: corrige importação e nome de função

--- src/main.py
+++ src/main.py
@@ -1,34 +1,35 @@
 import os
 from ingestion import extrair_e_limpar_pdf
 from preprocessing import pre_processar_sentencas
-from graph_model import vetorizar_e_calcular_similaridade, calcular_textrank
+
+from graph_model import construir_grafo, calcular_textrank
 from summarizer import gerar_resumo_extrativo
 
 def main():
     caminho_pdf = 'data/pdf/artigoDrogasImpactosNaCavidadeOral_ESSE1.pdf' 
-    caminho_saida = 'data/processed/resumo.txt'
+    caminho_saida = 'data/processed/resumo_final.txt'
     
     if not os.path.exists(caminho_pdf):
-        print(f"Erro: Arquivo '{caminho_pdf}' não encontrado. Verifique o caminho.")
+        print(f"Erro: Ficheiro '{caminho_pdf}' não encontrado. Verifique o caminho.")
         return
 
     print("Etapa 1: Ingestão e extração de dados...")
     sentencas_originais = extrair_e_limpar_pdf(caminho_pdf)
     print(f"{len(sentencas_originais)} sentenças extraídas.")
 
-    print("Etapa 2: Pré-processamento de PLN...")
+    print("Etapa 2: Pré-processamento...")
     sentencas_limpas = pre_processar_sentencas(sentencas_originais)
 
-    print("Etapa 3: Construção da Matriz de Adjacência do Grafo (SciPy)...")
-    matriz_adjacencia = vetorizar_e_calcular_similaridade(sentencas_limpas)
+    print("Etapa 3: Construção do Grafo (Python Puro / Lista de Adjacência)...")
+    grafo = construir_grafo(sentencas_limpas)
 
-    print("Etapa 4: Ranqueamento e Centralidade (TextRank)...")
-    scores = calcular_textrank(matriz_adjacencia)
+    print("Etapa 4: Ranqueamento e Centralidade...")
+    scores = calcular_textrank(grafo)
 
     print("Etapa 5: Seleção e geração do resumo...")
-    resumo = gerar_resumo_extrativo(sentencas_originais, scores, taxa_compressao=0.3)
+    resumo = gerar_resumo_extrativo(sentencas_originais, scores, taxa_compressao=0.3, formato="paragrafos")
 
-    print("Etapa 6: Resultado Salvo.")
+    print("Etapa 6: A guardar o resultado no ficheiro...")
     
     os.makedirs(os.path.dirname(caminho_saida), exist_ok=True)
     
